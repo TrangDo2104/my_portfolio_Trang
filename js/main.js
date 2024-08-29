5c9ea0c004a751a72ed895caa9e076ed377a38a6
TrangDo2104 <mtrang211104@gmail.com>
2024-08-29 15:31:58 +0800

    change many things

--- js/main.js
+++ js/main.js
@@ -219,7 +219,7 @@ sr.reveal('.project-bg', {interval: 100, origin: 'left'})
 
 /* TYPED ANIMATION */
 const typed = new Typed('.multiple-text', {
-  strings: ['Data Scientist', 'Business Analyst', 'Data Analyst'],
+  strings: ['Business Analyst', 'Process Engineer', 'Chemical Engineer', 'Data Analyst'],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
